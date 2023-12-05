5d790f9555fac9f2482b2fb56ddecd2134230c03
dinesh-here <dinesh.evil6@gmail.com>
2023-12-05 11:59:55 +0530

    Fixed readable files and errors

--- src/util_lib/util.rs
+++ src/util_lib/util.rs
@@ -1,9 +1,21 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, ops::Add};
 
 
 pub fn get_file_path(str: String, path:PathBuf) -> String {
-    return match str.as_str() {
+    let clean_path = clear_args(str);
+    return match clean_path.as_str() {
         "/" => String::from(path.to_string_lossy().to_string() + "index.html"),
-        _ => String::from(str)
+        _ => String::from(path.to_string_lossy().to_string()).add(&clean_path)
     };
+}
+
+
+fn clear_args(str: String) -> String {
+    let idx_pos = str.find("?").unwrap_or(0);
+
+    if idx_pos > 0 {
+        return str.split_at(idx_pos).0.to_string();
+    }
+
+    return str;
 }
